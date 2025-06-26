4c4cd369c3d2a5671168a079d27d2ebb4e98d9f8
trajrealm <trajrealm@trajrealm.com>
2025-06-26 17:00:50 -0400

    Render: allow render.com host in vite.config.ts

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ['talk2tables-webapp.onrender.com'],  // ✅ Add your Render domain here
+  },
+
 })
